50017dd8a1f474ea31f5cc8ae6fa28d2ea09163e
Kazem <itskazemk@gmail.com>
2024-08-05 18:59:59 +0330

    navbar child links

--- components/Layout/NavbarLinkGroup.tsx
+++ components/Layout/NavbarLinkGroup.tsx
@@ -18,15 +18,14 @@ export function LinksGroup({ icon: Icon, label, initiallyOpened, links }: LinksG
   const hasLinks = Array.isArray(links);
   const [opened, setOpened] = useState(initiallyOpened || false);
   const items = (hasLinks ? links : []).map((link) => (
-    <Text<"a">
-      component="a"
-      className={classes.link}
+    <a
+      className="block w-11/12 border-l-2 py-1 text-center hover:bg-gray-100"
       href={link.link}
       key={link.label}
       onClick={(event) => event.preventDefault()}
     >
       {link.label}
-    </Text>
+    </a>
   ));
 
   return (
@@ -34,15 +33,15 @@ export function LinksGroup({ icon: Icon, label, initiallyOpened, links }: LinksG
       <button onClick={() => setOpened((o) => !o)} className={`${classes.control} px-4 py-2`}>
         {/* <div className="grid grid-cols-4 w-full"> */}
         <div className="grid min-w-full grid-cols-[auto,1fr,20px] items-center">
-          <ThemeIcon variant="light" size={30} className="bg-blue-300">
+          <ThemeIcon variant="light" size={30} className="">
             <Icon style={{ width: rem(18), height: rem(18) }} />
           </ThemeIcon>
-          <Box ml="md" className="bg-purple-300">
+          <Box ml="md" className="">
             {label}
           </Box>
           {hasLinks && (
             <IconChevronRight
-              className={`${classes.chevron} bg-red-300`}
+              className={`${classes.chevron} `}
               stroke={1.5}
               style={{
                 width: rem(16),
